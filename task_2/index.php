2292b2d0d5f3323d8655d87accb74040c68d2a7c
Andrey Rubtsov <aa.rubtsov@icloud.com>
2025-07-15 15:02:56 +0300

    add task #2

---
+++ task_2/index.php
@@ -0,0 +1,21 @@
+<?php
+
+$fileName = basename(__FILE__);
+$lineNumber = __LINE__;
+
+echo "Имя файла >>> " . $fileName . "</br>" . "Номер строки >>> " . $lineNumber . "</br></br>";
+
+$str = <<<MY
+heredoc - синтаксис
+MY;
+
+echo $str . "</br></br>";
+
+
+$a="Рыба";
+
+$b="человек";
+
+echo "$a рыбою сыта, а $b человеком";
+
+?>
---
+++ task_3/index.php
@@ -0,0 +1,24 @@
+<?php
+
+function variableTypeDefinition($arg) {
+
+}
+
+$variable = 3.14;
+// $variable = 3;
+// $variable = 'one';
+// $variable = true;
+// $variable = null;
+// $variable = [];
+
+// if (is_bool($booleanVar)) {
+//     echo 'bool';
+// } else {
+//     echo 'not bool';
+// }
+
+
+$type = "string";
+
+echo "type is $type"
+?>
