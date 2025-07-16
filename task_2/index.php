dfc4f4c60227fc13120f4a1fd76d5ae9551d7e5f
Andrey Rubtsov <aa.rubtsov@icloud.com>
2025-07-16 17:10:43 +0300

    add task #3

--- task_2/index.php
+++ task_2/index.php
@@ -3,13 +3,13 @@
 $fileName = basename(__FILE__);
 $lineNumber = __LINE__;
 
-echo "Имя файла >>> " . $fileName . "</br>" . "Номер строки >>> " . $lineNumber . "</br></br>";
+echo "Имя файла >>> " . $fileName . "<br>" . "Номер строки >>> " . $lineNumber . "<br><br>";
 
 $str = <<<MY
 heredoc - синтаксис
 MY;
 
-echo $str . "</br></br>";
+echo $str . "<br><br>";
 
 
 $a="Рыба";
--- task_3/index.php
+++ task_3/index.php
@@ -1,24 +1,42 @@
 <?php
 
-function variableTypeDefinition($arg) {
+$choice = array(
+    "boolean" => "bool",
+    "double" => "float",
+    "integer" => "int",
+    "string" => "string",
+    "null" => "null",
+    "array" => "other",
+    "object" => "other",
+    "resource" => "other",
+);
 
+
+function variableTypeDefinition($value) {
+    $check = gettype($value);
+
+    return $check;
 }
 
-$variable = 3.14;
+
+function outputType($value) {
+    global $choice;
+
+    $variableType = variableTypeDefinition($value);
+
+    $key = strtolower(strval($variableType));
+
+    return $choice[$key];
+}
+
+
+// $variable = 3.14;
 // $variable = 3;
 // $variable = 'one';
 // $variable = true;
 // $variable = null;
-// $variable = [];
+$variable = [];
 
-// if (is_bool($booleanVar)) {
-//     echo 'bool';
-// } else {
-//     echo 'not bool';
-// }
+echo  "type is " . outputType($variable);
 
-
-$type = "string";
-
-echo "type is $type"
 ?>
