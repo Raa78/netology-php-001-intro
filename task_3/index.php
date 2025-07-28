226d21864aba3995067e392a1a3e3e0a68c2d11d
Andrey Rubtsov <aa.rubtsov@icloud.com>
2025-07-28 03:20:43 +0300

    editing task3

--- task_3/index.php
+++ task_3/index.php
@@ -1,42 +1,63 @@
 <?php
+$resultForIf = NULL;
 
-$choice = array(
-    "boolean" => "bool",
-    "double" => "float",
-    "integer" => "int",
-    "string" => "string",
-    "null" => "null",
-    "array" => "other",
-    "object" => "other",
-    "resource" => "other",
-);
+$resultForSwitch = NULL;
 
-
-function variableTypeDefinition($value) {
-    $check = gettype($value);
-
-    return $check;
-}
-
-
-function outputType($value) {
-    global $choice;
-
-    $variableType = variableTypeDefinition($value);
-
-    $key = strtolower(strval($variableType));
-
-    return $choice[$key];
-}
-
-
-$variable = 3.14;
+// $variable = true;
+// $variable = 3.14;
 // $variable = 3;
 // $variable = 'one';
-// $variable = true;
-// $variable = null;
+$variable = null;
 // $variable = [];
 
-echo  "type is " . outputType($variable);
+if (is_bool($variable)) {
+    $resultForIf = 'bool';
+} elseif (is_float($variable)) {
+    $resultForIf = 'float';
+} elseif (is_int($variable)) {
+    $resultForIf = 'int';
+} elseif (is_string($variable)) {
+    $resultForIf = 'string';
+} elseif (is_null($variable)) {
+    $resultForIf = 'null';
+} else {
+     $resultForIf = 'other';
+}
 
+echo  "type is " . $resultForIf . PHP_EOL;
+
+
+switch ($variable) {
+    case is_null($variable) === 1 && !isset($variable):
+        echo "enter is_null" . PHP_EOL;
+        $resultForSwitch = 'null';
+        break;
+    case is_bool($variable):
+        echo "enter is_bool" . PHP_EOL;
+        $resultForSwitch = 'bool';
+        break;
+    case is_float($variable):
+        $resultForSwitch = 'float';
+        break;
+    case is_int($variable):
+        $resultForSwitch = 'int';
+        break;
+    case is_string($variable):
+        $resultForSwitch = 'string';
+        break;
+    default:
+        $resultForSwitch = 'other';
+}
+
+// switch ($variable) {
+//     case is_bool($variable) || gettype($variable) !== NULL:
+//         echo "enter is_bool" . PHP_EOL;
+//         $resultForSwitch = 'bool';
+//         break;
+// }
+
+echo  "type is " . $resultForSwitch . PHP_EOL;
+
+var_dump("gettype>>>" . strval(gettype($variable)) . " / isset>>>" . isset($variable) . " / is_null>>>" . is_null($variable)) . PHP_EOL;
+var_dump(gettype($variable) === "NULL");
 ?>
